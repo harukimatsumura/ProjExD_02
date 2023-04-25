3221ae278c99aea471e54d76ad762d7ab804e358
haruki <c0b22136c1@edu.teu.ac.jp>
2023-04-25 17:41:48 +0900

    演習課題１未完成　#1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,14 +12,14 @@ def init():
     kk_img=pg.image.load("ex02/fig/3.png")
     kk_img=pg.transform.rotozoom(kk_img,0,2.0)
     kk_img={
-        (0,-1):pg.pg.transform.rotozoom(kk_img,270,1.0), #  追加未完成
-        (+1,-1):pg.transform.rotozoom(kk_img,315,1.0),#　追加未完成
-        (+1,0):pg.transform.rotozoom(kk_img,0,1.0),#　　追加未完成
-        (+1,+1):pg.transform.rotozoom(kk_img,45,1.0),#　追加未完成
-        (0,+1):pg.transform.rotozoom(kk_img,90,1.0),#　追加未完成
-        (-1,+1):pg.transform.rotozoom(kk_img,135,1.0),#　追加未完成
-        (-1,0):pg.transform.rotozoom(kk_img,180,1.0),#　追加未完成
-        (-1,-1):pg.transform.rotozoom(kk_img,225,1.0),#　追加未完成
+        (0,-1):pg.pg.transform.rotozoom(kk_img,270,1.0),  #  追加未完成
+        (+1,-1):pg.transform.rotozoom(kk_img,315,1.0), #　追加未完成
+        (+1,0):pg.transform.rotozoom(kk_img,0,1.0),  # 追加未完成
+        (+1,+1):pg.transform.rotozoom(kk_img,45,1.0),  # 追加未完成
+        (0,+1):pg.transform.rotozoom(kk_img,90,1.0),  # 追加未完成
+        (-1,+1):pg.transform.rotozoom(kk_img,135,1.0),  # 追加未完成
+        (-1,0):pg.transform.rotozoom(kk_img,180,1.0),  #　追加未完成
+        (-1,-1):pg.transform.rotozoom(kk_img,225,1.0),  # 追加未完成
     }
 
 
@@ -66,7 +66,7 @@ def main():
         pg_img.append(bb_img)
     avx,avy=vx*accs[min(tmr//1000,9)],vy*accs[min(tmr//100,9)]
     bb_img=bb_imgs[min(tmr//1000,9)]    
-"""
+    """
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
