0bb698cf1bdadbbbd3a58c0f3a55fb307f53c98d
haruki <c0b22136c1@edu.teu.ac.jp>
2023-04-25 15:40:49 +0900

    練習⑥

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,27 @@
 import random
 import pygame as pg
 import sys
+delta={
+    pg.K_UP:(0,-1),
+    pg.K_DOWN:(0,+1),
+    pg.K_LEFT:(-1,0),
+    pg.K_RIGHT:(+1,0),
+
+    }
+ 
+def check_bound(scr_rct:pg.Rect,obj_rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかトン、または爆弾SurfaceのRect
+    戻り値：横方向、縦方向のはみだし判定結果（画面：True/画面外：False）
+    """
+    yoko,tate=True,True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko=False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate=False
+    return yoko,tate       
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -9,6 +30,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=900,400
     tmr = 0
     bb_img=pg.Surface((20,20))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 爆弾の色、サイズ
@@ -26,12 +49,26 @@ def main():
                 return 0
 
         tmr += 1
+        key_lst=pg.key.get_pressed()
+        for k,mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+        if check_bound(screen.get_rect(),kk_rct) !=(True,True):
+            for k,mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0],-mv[1])        
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        #screen.blit(bb_img,[x,y])
+        #screen.blit(kk_img, [900,400])
+        screen.blit(kk_img,kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko,tate=check_bound(screen.get_rect(),bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1    
         screen.blit(bb_img,bb_rct)
-        
+        if kk_rct.colliderect(bb_rct):
+            return  #練習⑥
         pg.display.update()
         clock.tick(1000)
 
